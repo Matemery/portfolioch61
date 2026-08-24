52734f5cb3fa68680b02cdca17bea6f928222020
Mathew Emery <matemery123@gmail.com>
2026-08-24 11:25:25 -0400

    changed css and updated projects section

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,6 +3,8 @@ from portfolio import views
 
 urlpatterns = [
     path("", views.about_me_view, name="about_me"),
+    path("about", views.about_me_view, name="about_me"),
     path("experience/", views.experience_view, name= "experience"),
     path("contact/", views.contact_view, name="contact"),
+    
 ]
